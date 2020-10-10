871156c0f594cdb2ddacd4ae86c53f952b7fdc54
Torbjørn Fjørtoft <torbjorn@fjortoft.org>
2020-10-10 19:17:55 +0200

    refactor: typo

--- logging.go
+++ logging.go
@@ -7,13 +7,13 @@ import (
 	"time"
 )
 
-type wrappedReponseWriter struct {
+type wrappedResponseWriter struct {
 	status int
 	n      int
 	http.ResponseWriter
 }
 
-func (w *wrappedReponseWriter) Write(bytes []byte) (int, error) {
+func (w *wrappedResponseWriter) Write(bytes []byte) (int, error) {
 	n, err := w.ResponseWriter.Write(bytes)
 	if err != nil {
 		return n, err
@@ -22,14 +22,14 @@ func (w *wrappedReponseWriter) Write(bytes []byte) (int, error) {
 	return n, nil
 }
 
-func (w *wrappedReponseWriter) WriteHeader(statusCode int) {
+func (w *wrappedResponseWriter) WriteHeader(statusCode int) {
 	w.status = statusCode
 	w.ResponseWriter.WriteHeader(statusCode)
 }
 
 func RequestLogger(out io.Writer, h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		wr := &wrappedReponseWriter{200, 0, w}
+		wr := &wrappedResponseWriter{200, 0, w}
 		defer func(start time.Time) {
 			fmt.Fprintf(out, "ts=%s method=%s url=%s status=%d len=%d took=%s\n", start.Format(time.RFC3339), r.Method, r.URL, wr.status, wr.n, time.Since(start))
 		}(time.Now())
